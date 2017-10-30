49764a328e39dbb53d790ddac9c215271e5f374c
loconluis <luis.locon@gmail.com>
2017-10-30 13:52:58 -0600

    Moving renderCoutnerApp to playground

--- src/app.js
+++ src/app.js
@@ -17,34 +17,5 @@ const template = (
   </div>
 );
 
-//=================================================================================
-let count = 0;
-const addOne = () => {
-  count++;
-  renderCounterApp()
-}
-const minusOne = () => {
-  count--;
-  renderCounterApp()
-}
-const reset = () => {
-  count = 0
-  renderCounterApp()
-}
 
 const appRoot = document.getElementById('app')
-
-const renderCounterApp = () => {
-  const t2 = (
-    <div>
-      <h1>Count: {count}</h1>
-      <button onClick={addOne}>+1</button>
-      <button onClick={minusOne}>-1</button>
-      <button onClick={reset}>Reset</button>
-    </div>
-  );
-
-  ReactDOM.render(t2, appRoot);
-}
-
-renderCounterApp()
