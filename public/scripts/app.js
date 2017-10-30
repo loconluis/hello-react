4d7a2420cb4a71c344aa8a5d3c17924b4bc789f5
loconluis <luis.locon@gmail.com>
2017-10-30 14:14:33 -0600

    Looking forward on forms and buttons events

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,37 +4,80 @@ console.log('App.js is running!');
 
 var app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
+};
+
+var onFormSubmit = function onFormSubmit(e) {
+  e.preventDefault();
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option);
+    e.target.elements.option.value = "";
+
+    render();
+  }
+};
+
+var clearOptions = function clearOptions() {
+  app.options.length = 0;
+  render();
 };
 
 // JSX - Javascript XML
-var template = React.createElement(
-  'div',
-  null,
-  React.createElement(
-    'h1',
-    null,
-    app.title
-  ),
-  React.createElement(
-    'p',
-    null,
-    app.subtitle
-  ),
-  React.createElement(
-    'ol',
-    null,
-    React.createElement(
-      'li',
-      null,
-      'Item one'
-    ),
-    React.createElement(
-      'li',
-      null,
-      'Item two'
-    )
-  )
-);
+
 
 var appRoot = document.getElementById('app');
+
+var render = function render() {
+  var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      app.title
+    ),
+    app.subtitle && React.createElement(
+      'p',
+      null,
+      app.subtitle
+    ),
+    React.createElement(
+      'p',
+      null,
+      app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+      'ol',
+      null,
+      app.options.length > 0 ? app.options.map(function (option, index) {
+        return React.createElement(
+          'li',
+          { key: index },
+          option
+        );
+      }) : ""
+    ),
+    React.createElement(
+      'form',
+      { onSubmit: onFormSubmit },
+      React.createElement('input', { type: 'text', name: 'option' }),
+      React.createElement(
+        'button',
+        null,
+        'Add option'
+      )
+    ),
+    React.createElement(
+      'button',
+      { onClick: clearOptions },
+      'Clear'
+    )
+  );
+
+  ReactDOM.render(template, appRoot);
+};
+
+render();
--- src/app.js
+++ src/app.js
@@ -2,20 +2,50 @@ console.log('App.js is running!')
 
 const app = {
   title: 'Indecision App',
-  subtitle: 'Put your life in the hands of a computer'
+  subtitle: 'Put your life in the hands of a computer',
+  options: []
 };
 
+const onFormSubmit = (e) => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
+
+  if (option) {
+    app.options.push(option)
+    e.target.elements.option.value = "";
+
+    render()
+  }
+}
+
+const clearOptions = () => {
+  app.options.length = 0;
+  render()
+}
+
 // JSX - Javascript XML
-const template = (
-  <div>
-    <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
-    <ol>
-      <li>Item one</li>
-      <li>Item two</li>
-    </ol>
-  </div>
-);
 
 
-const appRoot = document.getElementById('app')
+const appRoot = document.getElementById('app');
+
+const render = () => {
+  const template = (
+    <div>
+      <h1>{app.title}</h1>
+      {app.subtitle && <p>{app.subtitle}</p>}
+      <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
+      <ol>
+        {app.options.length > 0 ? app.options.map((option, index) => <li key={index}>{option}</li>) : ""}
+      </ol>
+      <form onSubmit={onFormSubmit}>
+        <input type="text" name="option" />
+        <button>Add option</button>
+      </form>
+      <button onClick={clearOptions}>Clear</button>
+    </div>
+  );
+
+  ReactDOM.render(template, appRoot);
+}
+
+render();
