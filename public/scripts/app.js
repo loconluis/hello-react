4907252dd304491adc472ce732f0355320ba5f57
loconluis <luis.locon@gmail.com>
2017-10-30 12:17:38 -0600

    Learning how conditional rendering works

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -2,12 +2,78 @@
 
 console.log('App.js is running!');
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+
 // JSX - Javascript XML
 var template = React.createElement(
-  'p',
+  'div',
   null,
-  'Does this change'
+  React.createElement(
+    'h1',
+    null,
+    app.title
+  ),
+  React.createElement(
+    'p',
+    null,
+    app.subtitle
+  ),
+  React.createElement(
+    'ol',
+    null,
+    React.createElement(
+      'li',
+      null,
+      'Item one'
+    ),
+    React.createElement(
+      'li',
+      null,
+      'Item two'
+    )
+  )
 );
+
+var user = {
+  name: 'Luis Locon',
+  age: 24,
+  location: 'Guatemala'
+};
+
+function getLocation(location) {
+  if (location) {
+    return React.createElement(
+      'p',
+      null,
+      'Location: ',
+      location
+    );
+  }
+}
+
+// Create a templateTwo var JSX EXPRESSION
+var templateTwo = React.createElement(
+  'div',
+  null,
+  React.createElement(
+    'h1',
+    null,
+    user.name ? user.name : 'Anonymous'
+  ),
+  ' ',
+  user.name && user.age >= 18 && React.createElement(
+    'p',
+    null,
+    'Age: ',
+    user.age
+  ),
+  ' ',
+  getLocation(user.location)
+);
+
 var appRoot = document.getElementById('app');
 
-ReactDOM.render(template, appRoot);
+ReactDOM.render(templateTwo, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,44 @@
 console.log('App.js is running!')
 
+var app = {
+  title: 'Indecision App',
+  subtitle: 'Put your life in the hands of a computer'
+};
+
 // JSX - Javascript XML
-var template = <p>Does this change</p>
+var template = 
+  (<div>
+    <h1>{app.title}</h1>
+    <p>{app.subtitle}</p>
+    <ol>
+      <li>Item one</li>
+      <li>Item two</li>
+    </ol>
+  </div>
+  );
+
+var user = {
+  name: 'Luis Locon',
+  age: 24,
+  location: 'Guatemala'
+}
+
+function getLocation(location) {
+  if(location) {
+    return <p>Location: {location}</p>
+  }
+}
+
+// Create a templateTwo var JSX EXPRESSION
+var templateTwo = (
+  <div>
+    <h1>{user.name ? user.name : 'Anonymous'}</h1> {/*Operador ternario IF STATMENT*/}
+    {(user.name && user.age >= 18) && <p>Age: {user.age}</p>} {/*Rigth value is used when is true*/}
+    {getLocation(user.location)}
+  </div>
+);
+
 var appRoot = document.getElementById('app')
 
 
-ReactDOM.render(template, appRoot)
+ReactDOM.render(templateTwo, appRoot)
