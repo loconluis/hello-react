99aa1bac1909bd065efcc3c1e4db763567396389
loconluis <luis.locon@gmail.com>
2017-11-05 00:45:39 -0600

    We set babel to load with webpack, create babelrc to set what preset we gonna use

---
+++ src/utils.js
@@ -0,0 +1,10 @@
+console.log('Utilis.js is running');
+
+const square = x => x * x;
+
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+// named exports
+export { square, add, subtract }
